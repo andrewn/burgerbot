02d3b7086b287988528359ec7702f591f08894d6
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2015-11-13 20:20:18 +0100

    Bind to port

--- burgerbot.js
+++ burgerbot.js
@@ -36,7 +36,7 @@ app.get('/', function (req, res) {
   res.send(html);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
